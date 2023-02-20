0acf4886d311293cd29d5dbe1c46a7689abb7081
a-tkachevaaa <sweetnightowl5@gmail.com>
2023-02-20 13:26:21 +0300

    hw2 js code

---
+++ index.js
@@ -0,0 +1,75 @@
+
+//Задание 10
+const nameUser=prompt("Как вас зовут?");
+alert (`Привет, ${nameUser}!`)
+
+//Задание 9.4
+let info=prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info]);
+
+//Задание 9.3 
+delete user ["city of residence"];
+
+//Задание 9.2 
+user ["age"]=20;
+
+//Задание 9.1 
+user ["city of residence"]=Novorossiysk;
+
+//Задание 9.0 
+let user = {
+	name: 'Aleksandra',
+	age: 24,
+	isAdmin: true,
+}
+
+//Задание 8
+let age=prompt("Сколько вам лет?");
+alert(age);
+
+
+//Задание 7
+let num = 1;
+num +=5;
+num -=3;
+num *=7;
+num /=3;
+num +=1;
+num -=1;
+alert(num);
+
+
+/*Задание 6
+let a=9;
+let b=2;
+let result=alert(a%b);
+*/
+
+/*Задание 5
+let numberTwo=(2);
+let result=alert(numberTwo**5);
+*/
+
+//Задание 4
+let number10=10;
+let number2=2;
+let resultPlus=alert(number10+number2);
+let resultMinus=alert(number10-number2);
+let resultMultiply=alert(number10*number2);
+let resultDivision=alert(number10/number2);
+
+
+//Задание 3 
+const creator=alert("Brendan Eich");
+
+//Задание 2 
+const year=alert(2007);
+
+/*Задание 1 
+Let a=10;
+Alert(a);
+a=20; 
+Alert (a);
+*/
+
+
