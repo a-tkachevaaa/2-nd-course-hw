8a7ef3a2a0f03fa2f19c8511bf93af316c1a39de
a-tkachevaaa <sweetnightowl5@gmail.com>
2023-02-26 17:27:03 +0300

    hw3 2.3 Условное ветвление

--- index.js
+++ index.js
@@ -1,34 +1,152 @@
 
-//Задание 10
-const nameUser=prompt("Как вас зовут?");
-alert (`Привет, ${nameUser}!`)
+//2.3 Условное ветвление 
 
-//Задание 9.4
-let info=prompt("Какую информацию хотите узнать о пользователе?");
-alert(user[info]);
-
-//Задание 9.3 
-delete user ["city of residence"];
-
-//Задание 9.2 
-user ["age"]=20;
-
-//Задание 9.1 
-user ["city of residence"]=Novorossiysk;
-
-//Задание 9.0 
-let user = {
-	name: 'Aleksandra',
-	age: 24,
-	isAdmin: true,
+/*Задание 1
+let password=("пароль");
+let enterPassword=prompt("Введите пароль");
+if (enterPassword===password) {
+	alert ("Пароль введен верно");
+} else {
+	alert("Пароль введен неправильно");
 }
 
-//Задание 8
+/*Задание 2
+let c=prompt("Введите число");
+if (c>0 && c<10) {
+	console.log ("Верно");
+	} 
+else {
+	console.log ("Неверно");
+}
+*/
+
+/*Задание 3
+let d=prompt("Введите первое число");
+let e=prompt("Введите второе число");
+if (d>100 || e>100) {
+	console.log ("Верно");
+	} 
+else {
+	console.log ("Неверно");
+}
+*/
+
+/*Задание 4
+let a =  Number('2');
+let b = Number('3');
+alert(a + b);
+*/
+
+
+
+/*Задание 5
+let mounth = prompt("Укажите месяц");
+  switch (mounth) {
+    case "1":
+      alert("Зима");
+      break;
+    case "2":
+      alert("Зима");
+      break;
+    case "3":
+      alert("Весна");
+      break;
+    case "4":
+      alert("Весна");
+      break;
+    case "5":
+      alert("Весна");
+      break;
+    case "6":
+      alert("Лето");
+      break;
+    case "7":
+      alert("Лето");
+      break;
+    case "8":
+      alert("Лето");
+      break;
+    case "9":
+      alert("Осень");
+      break;
+    case "10":
+      alert("Осень");
+      break;
+    case "11":
+      alert("Осень");
+      break;
+    case "12":
+      alert("Зима");
+      break;
+    default:
+      alert("Неверно");
+      break;
+  }
+*/
+
+/*Задание 8
+let clientOS=Number(prompt("Если у вас Android - 1, если iOS - 0"));
+if (clientOS===1) {
+  console.log("Установите версию приложения для Android по ссылке");
+} else if (clientOS===0) {
+  console.log("Установите версию приложения для iOS по ссылке");
+}
+else {
+  console.log ("Указали неверное число");
+}
+*/
+
+/*Задание 9
+let clientOS=Number(prompt("Если у вас Android - 1, если iOS - 0"));
+let clientDeviceYear=Number(prompt("Укажите год выпуска вашего устройства"));
+if (clientOS===1 && clientDeviceYear>=2015) {
+  alert("Установите версию приложения для Android по ссылке");
+} else if (clientOS===0 && clientDeviceYear>=2015) {
+  alert("Установите версию приложения для iOS по ссылке");
+}
+else if (clientOS===1 && clientDeviceYear<=2015) {
+  alert ("Установите облегченную версию приложения для Android по ссылке");
+}
+else if (clientOS===0 && clientDeviceYear<=2015) {
+  alert ("Установите облегченную версию приложения для iOS по ссылке");
+}
+else {
+  alert ("Указали неверное число");
+}
+*/
+
+//2.2 Переменные. Типы данных
+
+/*Задание 10
+const nameUser=prompt("Как вас зовут?");
+alert (`Привет, ${nameUser}!`)
+*/
+
+/*Задание 9.4 
+let info=prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info]);
+*/
+
+//Задание 9.3 delete user ["city of residence"];
+
+//Задание 9.2 user ["age"]=20;
+
+//Задание 9.1 user ["city of residence"]=Novorossiysk;
+
+/*Задание 9.0 
+let user = {
+	name: 'Aleksandra',
+	age: 24, 
+	isAdmin: true,
+}
+*/
+
+/*Задание 8 
 let age=prompt("Сколько вам лет?");
 alert(age);
+*/
 
-
-//Задание 7
+/*Задание 7
 let num = 1;
 num +=5;
 num -=3;
@@ -37,7 +155,7 @@ num /=3;
 num +=1;
 num -=1;
 alert(num);
-
+*/
 
 /*Задание 6
 let a=9;
@@ -50,20 +168,18 @@ let numberTwo=(2);
 let result=alert(numberTwo**5);
 */
 
-//Задание 4
+/*Задание 4
 let number10=10;
 let number2=2;
 let resultPlus=alert(number10+number2);
 let resultMinus=alert(number10-number2);
 let resultMultiply=alert(number10*number2);
 let resultDivision=alert(number10/number2);
+*/
 
+//Задание 3 const creator=alert("Brendan Eich");
 
-//Задание 3 
-const creator=alert("Brendan Eich");
-
-//Задание 2 
-const year=alert(2007);
+//Задание 2 const year=alert(2007);
 
 /*Задание 1 
 Let a=10;
